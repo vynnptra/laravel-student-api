362e60df7144f0a481c2336c56313257fcca8375
vynnptra <mryu.uiiiii@gmail.com>
2025-04-08 18:47:38 +0700

    feature create hobby

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\HobbyController;
 use App\Http\Controllers\Api\SiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('siswa', SiswaController::class);
+Route::apiResource('hobby', HobbyController::class);
